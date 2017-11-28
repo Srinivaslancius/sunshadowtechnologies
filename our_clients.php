43ee8565e277f876a5103764a22a679d1f4a7dea
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-28 11:35:39 +0530

    changes in index file

--- our_clients.php
+++ our_clients.php
@@ -1,18 +1,18 @@
-<div class="auto-container">     	
+<div class="auto-container">        
             <div class="sec-title text-center">
                 <h2>Our <span class="normal-font theme_color">Clients</span></h2><br>
-        	<div class="row clearfix">               
+            <div class="row clearfix">               
               
              <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                	<article class="inner-box wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-                		<figure class="image-box">
-                        	<a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                    <article class="inner-box wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
                           
                         </figure>
                         <div class="content-box">
-                        	
-                        	<div class="post-info clearfix">
-                            	<div class="post-author">Japan</div>
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Japan</div>
                                 
                             </div>
                            
@@ -21,27 +21,122 @@
                 </div>
                 
               
-                  
-                
-               
-                   <?php $getFeaturesData = getAllDataCheckActive('our_clients',0);?>
-                <?php while ($row = $getFeaturesData->fetch_assoc()) { ?>
-				 <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                	<article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
-                		<figure class="image-box">
-                        	<a href="#"><img src="<?php echo $base_url . 'uploads/our_clients_images/'.$row['image'] ?>" alt=""></a>
+                  <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
                             
                         </figure>
                         <div class="content-box">
-                        	
-                        	<div class="post-info clearfix">
-                            	<div class="post-author"><?php echo $row['title']; ?></div>
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Taiwan NO.6986</div>
+                                
+                            </div>
+                           
+                        </div>
+                    </article>
+                </div>
+                
+               
+                   <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                          
+                        </figure>
+                        <div class="content-box">
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Japan</div>
+                                
+                            </div>
+                           
+                        </div>
+                    </article>
+                </div>
+                 <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                            
+                        </figure>
+                        <div class="content-box">
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Taiwan NO.6986</div>
                                
                             </div>
                             
                         </div>
                     </article>
-                </div> 
-                <?php } ?> 
+                </div>
+                 <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                           
+                        </figure>
+                        <div class="content-box">
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Japan</div>
+                                
+                            </div>
+                           
+                        </div>
+                    </article>
+                </div>
+                <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                           
+                        </figure>
+                        <div class="content-box">
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Japan</div>
+                                
+                            </div>
+                           
+                        </div>
+                    </article>
+                </div>
+                <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                           
+                        </figure>
+                        <div class="content-box">
+                            
+                            <div class="post-info clearfix">
+                                <div class="post-author">Japan</div>
+                                
+                            </div>
+                           
+                        </div>
+                    </article>
+                </div>
+                 <div class="column blog-news-column col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                    <article class="inner-box wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                        <figure class="image-box">
+                            <a href="#"><img src="images/slides/5.jpg" alt=""></a>
+                           
+                        </figure>
+                        <div class="content-box">
+                            <!--<h3><a href="#">Save the animals</a></h3>-->
+                            <div class="post-info clearfix">
+                            <div class="post-author">Taiwan NO.6986</div>
+                               
+                            </div>
+                        <!--    <div class="text-justify">Lorem ipsum dolor sit amet, consectetur adipi elit sed do eiusmod tempor incididunt ut modo labore et dolore magna aliqua veniam...</div>
+                            <a href="#" class="theme-btn read-more">Read More</a>-->
+                        </div>
+                    </article>
+                </div>
+                
+                
             </div>
         </div>
