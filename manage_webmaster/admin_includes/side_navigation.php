c87d0c67f36c6bf08eb7d5151bed2cb715f547b2
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-29 12:38:29 +0530

    changes in menu file

--- manage_webmaster/admin_includes/side_navigation.php
+++ manage_webmaster/admin_includes/side_navigation.php
@@ -141,59 +141,8 @@
                 <span class="menu-text">News</span>
               </a>
             </li>
-            <li  class="<?php if($page_name == 'association.php' || $page_name == 'add_association.php' || $page_name == 'edit_association.php') { echo "active"; } ?>">
-              <a href="association.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-collection-item  zmdi-hc-fw"></i>
-                </span> 
-                <span class="menu-text">Association</span>
-              </a>
-            </li>
-            <!-- <li class="with-sub">
-              <a href="#" aria-haspopup="true">
-                <span class="menu-icon">
-                  <i class="zmdi zmdi-account-circle zmdi-hc-fw"></i>
-                </span>
-                <span class="menu-text">Careers</span>
-              </a>
-              <ul class="sidebar-submenu collapse">
-                <li class="menu-subtitle">Careers</li>
-                <li class="<?php if($page_name == 'current_openings.php' || $page_name == 'add_current_openings.php' || $page_name == 'edit_current_openings.php') { echo "active"; } ?>"><a href="current_openings.php">Current Openings</a></li> 
-                <li class="<?php if($page_name == 'posted_resumes.php' || $page_name == 'add_posted_resumes.php' || $page_name == 'edit_posted_resumes.php') { echo "active"; } ?>"><a href="posted_resumes.php">Posted Resumes</a></li>
-              </ul>
-            </li> -->
-            <!-- <li  class="<?php if($page_name == 'testimonials.php' || $page_name == 'add_testominials.php' || $page_name == 'edit_testimonials.php') { echo "active"; } ?>">
-              <a href="testimonials.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-comments  zmdi-hc-fw"></i>
-                </span> 
-                <span class="menu-text">Testimonials</span>
-              </a>
-            </li> -->
-            <li  class="<?php if($page_name == 'services.php' || $page_name == 'add_services.php' || $page_name == 'edit_services.php') { echo "active"; } ?>">
-              <a href="services.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-wrench zmdi-hc-fw"></i>
-                </span> 
-                <span class="menu-text">Services</span>
-              </a>
-            </li>
-            <li  class="<?php if($page_name == 'partnerships.php') { echo "active"; } ?>">
-              <a href="partnerships.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-comments  zmdi-hc-fw"></i>
-                </span> 
-                <span class="menu-text">Partnerships</span>
-              </a>
-            </li>
-            <li  class="<?php if($page_name == 'our_history.php' || $page_name == 'add_our_history.php' || $page_name == 'edit_our_history.php') { echo "active"; } ?>">
-              <a href="our_history.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-collection-image  zmdi-hc-fw"></i>
-                </span> 
-                <span class="menu-text">Our History</span>
-              </a>
-            </li>
+            
+          
             <li  class="<?php if($page_name == 'faqs.php' || $page_name == 'add_faqs.php' || $page_name == 'edit_faqs.php') { echo "active"; } ?>">
               <a href="faqs.php" aria-haspopup="true">
                 <span class="menu-icon">
@@ -202,14 +151,7 @@
                 <span class="menu-text">FAQ'S</span>
               </a>
             </li>
-            <li  class="<?php if($page_name == 'faq_categories.php' || $page_name == 'add_faq_categories.php' || $page_name == 'edit_faq_categories.php') { echo "active"; } ?>">
-              <a href="faq_categories.php" aria-haspopup="true">
-                <span class="menu-icon">
-                   <i class="zmdi zmdi-store zmdi-hc-fw"></i>
-                </span>
-                <span class="menu-text">Faq Categories</span>
-              </a>
-            </li>
+            
             <li  class="<?php if($page_name == 'newsletter.php' ) { echo "active"; } ?>">
               <a href="newsletter.php" aria-haspopup="true">
                 <span class="menu-icon">
