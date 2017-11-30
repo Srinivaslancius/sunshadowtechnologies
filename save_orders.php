4091215fbe423fab5ea0d768a3b50ff91eec320d
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-30 10:46:40 +0530

    changes in save orders file

--- save_orders.php
+++ save_orders.php
@@ -32,7 +32,7 @@ if(isset($_POST["submit"]) && $_POST["submit"]!="") {
 	</body>
 	</html>
 	";
-	echo $message; die;
+	
 	// Always set content-type when sending HTML email
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
